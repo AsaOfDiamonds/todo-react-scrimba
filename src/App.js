cfe63a70907dacd99e5bed3a0bb47fe1e18d2196
1@$@1github <asaofdiamonds@gmail.com>
2018-12-27 10:35:52 -0700

    added fake data and mapped over data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import TodoItem from "./Components/TodoItem";
+import todosData from "./todosData";
 import './App.css';
 
 
 class App extends Component {
   render() {
+      const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
     return (
       <div className="App">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />        
+            {todoItems}
       </div>
     );
   }
